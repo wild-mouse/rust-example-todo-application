f0d4a758dac987f903e62572874f53c1d2aa72c7
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-09-23 11:11:13 +0900

    Add endpoints for each methods

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -1,12 +1,48 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/tasks")]
+fn get_tasks() -> &'static str {
+    "Going to be able to get all tasks."
+}
+
+#[get("/tasks/<id>")]
+fn get_task(id: String) -> &'static str {
+    "Going to be able to get single task."
+}
+
+#[post("/tasks")]
+fn add_task() -> &'static str {
+    "Going to be able to add task."
+}
+
+#[put("/tasks/<id>")]
+fn update_task(id: String) -> &'static str {
+    "Going to be able to update task."
+}
+
+#[delete("/tasks/<id>")]
+fn delete_task(id: String) -> &'static str {
+    "Going to be able to delete task."
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/",
+               routes![
+                index,
+                 get_tasks,
+                  get_task,
+                   add_task,
+                    update_task,
+                     delete_task,
+                ])
+        .launch();
 }
